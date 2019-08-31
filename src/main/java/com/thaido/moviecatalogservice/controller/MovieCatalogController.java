763601f75ddfd89da144e8c9b8b4b4d87864b25b
thai <thai.do@codelink.io>
2019-08-31 14:57:52 +0700

    Add Movie catalog controller

---
+++ src/main/java/com/thaido/moviecatalogservice/controller/MovieCatalogController.java
@@ -0,0 +1,48 @@
+package com.thaido.moviecatalogservice.controller;
+
+import com.thaido.moviecatalogservice.model.CatalogItem;
+import com.thaido.moviecatalogservice.model.Movie;
+import com.thaido.moviecatalogservice.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog/")
+public class MovieCatalogController {
+
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @RequestMapping("/{userId}")
+  public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+    RestTemplate restTemplate = new RestTemplate();
+    List<Rating> ratings = Arrays.asList(
+      new Rating("1234" , 4),
+      new Rating("5678" , 5)
+    );
+
+    return ratings.stream()
+      .map(
+        rating ->
+        {
+          Movie movie = restTemplate.getForObject(
+            "http://localhost:8081/movies/" + rating.getMovieId(),
+            Movie.class
+          );
+          return new CatalogItem(
+            movie.getName(),
+            "Test",
+            rating.getRating()
+          );
+        }
+      )
+      .collect(Collectors.toList());
+  }
+}
