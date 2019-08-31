1d164766965344d8c24225a627b580c4ad436182
thai <thai.do@codelink.io>
2019-08-31 17:31:07 +0700

    Call rating data service to get ratings

--- src/main/java/com/thaido/moviecatalogservice/controller/MovieCatalogController.java
+++ src/main/java/com/thaido/moviecatalogservice/controller/MovieCatalogController.java
@@ -3,12 +3,15 @@ package com.thaido.moviecatalogservice.controller;
 import com.thaido.moviecatalogservice.model.CatalogItem;
 import com.thaido.moviecatalogservice.model.Movie;
 import com.thaido.moviecatalogservice.model.Rating;
+import com.thaido.moviecatalogservice.model.UserRatings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,15 +23,20 @@ public class MovieCatalogController {
   @Autowired
   private RestTemplate restTemplate;
 
+  @Autowired
+  private WebClient.Builder webClient;
+
   @RequestMapping("/{userId}")
   public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-    RestTemplate restTemplate = new RestTemplate();
-    List<Rating> ratings = Arrays.asList(
-      new Rating("1234" , 4),
-      new Rating("5678" , 5)
+    WebClient.Builder builder = WebClient.builder();
+
+    UserRatings ratings = restTemplate.getForObject(
+      "http://localhost:8082/rating/user/" + userId,
+      UserRatings.class
     );
 
-    return ratings.stream()
+    return ratings.getUserRatings()
+      .stream()
       .map(
         rating ->
         {
